4ef266db8f58312ecb7cef59b14cddb280700472
LeopoldoWall <leopoldo.wall@inacapmail.cl>
2023-09-21 08:41:15 -0300

    vista app1 creada

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def display(request):
+    return(HttpResponse("<h1> Hola soy la app1 </h1>"))
