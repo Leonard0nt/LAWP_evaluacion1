1c131c3dcf3d9962b20f1fad594f5eec07c810f5
LeopoldoWall <leopoldo.wall@inacapmail.cl>
2023-09-21 08:58:32 -0300

    app2 funcionando en navegador

--- Evaluacion_1/urls.py
+++ Evaluacion_1/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app2 import views as app2
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('app2/', app2.display)
 ]
